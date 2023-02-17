847f8c9ba54ca0cf6009466290cd7c1f812cb13b
ReTims <renatatims@gmail.com>
2023-02-17 15:21:50 -0600

    Included Get /users route - to display all users' thoughts

--- server/routes/user-routes.js
+++ server/routes/user-routes.js
@@ -10,3 +10,24 @@ AWS.config.update(awsConfig);
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 //Table value - Thoughts
 const table = 'Thoughts';
+
+//ROUTES
+
+//GET /users - route - retrieve all user's thoughts
+router.get('/users', (req, res) => {
+    const params = {
+      TableName: table,
+    };
+    // Scan return all items in the table
+    dynamodb.scan(params, (err, data) => {
+      if (err) {
+        res.status(500).json(err); // an error occurred
+      } else {
+        res.json(data.Items);
+      }
+    });
+  });
+
+
+module.exports = router;
+
