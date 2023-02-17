6e46e654a2b524d0979ec5b6ef4e5adfe21e920b
ReTims <renatatims@gmail.com>
2023-02-17 14:54:30 -0600

    Added loop to all users to create the params object + call to the database Dynamodb- LoadThoughts.js file completed

--- server/db/LoadThoughts.js
+++ server/db/LoadThoughts.js
@@ -1,21 +1,43 @@
 // imports aws-sdk package
-const AWS = require('aws-sdk');
+const AWS = require("aws-sdk");
 
 //import fs package
-const fs = require('fs');
-
+const fs = require("fs");
 
 AWS.config.update({
-    region: 'us-east-2',
-  });
-  //DocumentClient () class to create dynamodb service object
-  const dynamodb = new AWS.DynamoDB.DocumentClient({
-    apiVersion: '2012-08-10',
-  });
+  region: "us-east-2",
+});
+//DocumentClient () class to create dynamodb service object
+const dynamodb = new AWS.DynamoDB.DocumentClient({
+  apiVersion: "2012-08-10",
+});
 
-
-console.log('Importing thoughts into DynamoDB. Please wait.');
+console.log("Importing thoughts into DynamoDB. Please wait.");
 const allUsers = JSON.parse(
-  fs.readFileSync('./server/seed/users.json', 'utf8'),
+  fs.readFileSync("./server/seed/users.json", "utf8")
 );
-  
+
+//Loop over the allUsers array and create the params object with the elements in the array
+allUsers.forEach((user) => {
+  const params = {
+    TableName: "Thoughts",
+    Item: {
+      username: user.username,
+      createdAt: user.createdAt,
+      thought: user.thought,
+    },
+  };
+  //Call to the database - dynamodb - PUT method
+  dynamodb.put(params, (err, data) => {
+    if (err) {
+      console.error(
+        "Unable to add thought",
+        user.username,
+        ". Error JSON:",
+        JSON.stringify(err, null, 2)
+      );
+    } else {
+      console.log("PutItem succeeded:", user.username);
+    }
+  });
+});
