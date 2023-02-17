0d85b77b91f9f4b221691dd61d1811f2e1f5fbb3
ReTims <renatatims@gmail.com>
2023-02-17 14:11:26 -0600

    DynamoDB instance call to create the table

--- server/db/CreateThoughtsTable.js
+++ server/db/CreateThoughtsTable.js
@@ -27,3 +27,19 @@ const params = {
     },
   };
 
+  //Call the DynamoDB Instance to create the Table
+  
+  dynamodb.createTable(params, (err, data) => {
+    if (err) {
+      console.error(
+        'Unable to create table. Error JSON:',
+        JSON.stringify(err, null, 2),
+      );
+    } else {
+      console.log(
+        'Created table. Table description JSON:',
+        JSON.stringify(data, null, 2),
+      );
+    }
+  });
+
