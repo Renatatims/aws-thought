1d58a02cb0bea7081bef0bf92847bbd5b49b6843
ReTims <renatatims@gmail.com>
2023-02-21 11:38:47 -0600

    Included upload image button + handleImageUpload function on ThoughtForm index.js

--- client/src/components/ThoughtForm/index.js
+++ client/src/components/ThoughtForm/index.js
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from 'react';
+//initial value of fileInput set to null - this ensures that the reference to the DOM element is current
+const fileInput = useRef(null);
 
 const ThoughtForm = () => {
   const [formState, setFormState] = useState({
@@ -21,11 +23,11 @@ const ThoughtForm = () => {
 
     //POST - new thought - Fetch function - send the form data to the endpoint in the body of the request
     const postData = async () => {
-      const res = await fetch('/api/users', {
-        method: 'POST',
+      const res = await fetch("/api/users", {
+        method: "POST",
         headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
+          Accept: "application/json",
+          "Content-Type": "application/json",
         },
         body: JSON.stringify(formState),
       });
@@ -34,10 +36,18 @@ const ThoughtForm = () => {
     };
     postData();
 
-    
     // clear form value
     setFormState({ username: "", thought: "" });
     setCharacterCount(0);
+  };
+  //handleImageUpload function - Upload image button - retrieves the image file uploaded by the user 
+  const handleImageUpload = (event) => {
+    //preventDefault - so the image can be uploaded before the thought is saved
+    event.preventDefault();
+    const data = new FormData();
+    data.append('image', fileInput.current.files[0]);
+    // send image file to endpoint with the postImage function
+    // ...
   };
 
   return (
@@ -63,6 +73,13 @@ const ThoughtForm = () => {
           className="form-input col-12 "
           onChange={handleChange}
         ></textarea>
+        <label className="form-input col-12  p-1">
+          Add an image to your thought:
+          <input type="file" ref="{fileInput}" className="form-input p-2" />
+          <button className="btn" onClick="{handleImageUpload}" type="submit">
+            Upload
+          </button>
+        </label>
         <button className="btn col-12 " type="submit">
           Submit
         </button>
