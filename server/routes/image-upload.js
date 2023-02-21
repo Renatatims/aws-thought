d825b3f6b6f31cc40f354550ed4db6823a4b1a04
ReTims <renatatims@gmail.com>
2023-02-21 10:33:34 -0600

    Set-up the image route on image-route.js

--- server/routes/image-upload.js
+++ server/routes/image-upload.js
@@ -3,3 +3,24 @@ const express = require('express');
 const router = express.Router();
 const multer = require('multer');
 const AWS = require('aws-sdk');
+
+//Instantiate the service object - s3 - to communicate with the S3 web service - so the image can be uploaded to the S3 bucket
+const s3 = new AWS.S3({
+    apiVersion: '2006-03-01',//locked the API version - lower chance of breaking 
+  });
+
+//temporary storage container that will hold the image files
+const storage = multer.memoryStorage({
+    destination: function (req, file, callback) {
+      callback(null, '');
+    },
+  });
+
+// upload object - single method defines that this upload function will only receive 1 image - "image" is the key
+const upload = multer({ storage }).single('image');
+
+//Image upload Route
+router.post('/image-upload', upload, (req, res) => {
+    // set up params config
+    // set up S3 service call
+  });
