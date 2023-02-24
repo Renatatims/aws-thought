78890074e8514a86c3d921f959b8f220211fe9af
ReTims <renatatims@gmail.com>
2023-02-24 12:46:24 -0600

    s3Bucket name variable

--- server/utils/params-config.js
+++ server/utils/params-config.js
@@ -1,11 +1,12 @@
 const { v4: uuidv4 } = require("uuid");
+const s3BucketName = process.env.REACT_APP_S3_BUCKET_NAME;
 
 const params = (fileName) => {
   const myFile = fileName.originalname.split(".");
   const fileType = myFile[myFile.length - 1];
   //params config
   const imageParams = {
-    Bucket: config.bucket,
+    Bucket: s3BucketName,
     Key: `${uuidv4()}.${fileType}`,
     Body: fileName.buffer,
     // allow read access to this file
